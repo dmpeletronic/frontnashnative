2e0df36dfb93fbc02b2b989d1fcaccdfa2bd9f9b
GustavoLpMa <gustavolpma04@gmail.com>
2022-07-20 09:28:41 -0300

    Container and Text color modification

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>NashBet APP. Sua melhor aposta!</Text>
+      <StatusBar style="inverted" />
     </View>
   );
 }
@@ -14,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#006400',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+    
 });
